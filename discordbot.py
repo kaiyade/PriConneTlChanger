970cd661fd7cd192211196b106a5c74426ffdf41
kaiyade <61071830+kaiyade@users.noreply.github.com>
2021-09-27 18:49:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,10 +17,14 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 
 
 
+scoremathuser = []
+d = 0
+
 @bot.event
 async def on_message(message):
     if message.author.bot:
         return
+    global scoremathuser, d
 
     try:
         if str(message.content)[0:2] == "tl":
@@ -103,10 +107,18 @@ async def on_message(message):
 
         elif str(message.content)[0:2] == "= ":
             try:
-                temp = message.content[2:].replace('×', '*')
-                temp = temp.replace('x', '*')
-                temp = temp.replace('÷', '/')
-                math = await message.channel.send(eval(temp))
+                if "スコア計算" in message.content:
+                    spl = message.content.split()
+                    await message.channel.send("```<段階> <ボス(整数)> <ダメージ>```")
+                    if int(message.author.id) in scoremathuser:
+                        pass
+                    else:
+                        scoremathuser.append(int(message.author.id))
+                else:
+                    temp = message.content[2:].replace('×', '*')
+                    temp = temp.replace('x', '*')
+                    temp = temp.replace('÷', '/')
+                    math = await message.channel.send(eval(temp))
             except:
                 pass
 
@@ -122,11 +134,102 @@ async def on_message(message):
 
         elif str(message.content)[0:6] == ";;help":
             try:
-                await message.channel.send("```tl <秒数> - TLを変換\n" + "= <計算式> - 計算```")
+                await message.channel.send("```tl <秒数> - TLを変換\n" + "= <計算式> - 計算\n" + "= スコア計算 - クラバトのスコアの計算```")
                 
             except:
                 pass
-        
+
+        else:
+            if int(message.author.id) in scoremathuser:
+                msg = message.content
+
+                msg = msg.replace(",", "")
+                scorelist = []
+
+                if "\n" in  msg:
+                    spline = msg.split("\n")
+                else:
+                    spline = [msg]
+                amount = 0
+                
+                for line in spline:
+                    item = line.split()
+
+                    if int(item[0]) == 1:
+                        if int(item[1]) == 1:
+                            d = 1.2
+                        elif int(item[1]) == 2:
+                            d = 1.2
+                        elif int(item[1]) == 3:
+                            d = 1.3
+                        elif int(item[1]) == 4:
+                            d = 1.4
+                        elif int(item[1]) == 5:
+                            d = 1.5
+
+                    elif int(item[0]) == 2:
+                        if int(item[1]) == 1:
+                            d = 1.6
+                        elif int(item[1]) == 2:
+                            d = 1.6
+                        elif int(item[1]) == 3:
+                            d = 1.8
+                        elif int(item[1]) == 4:
+                            d = 1.9 
+                        elif int(item[1]) == 5:
+                            d = 2.0
+
+                    elif int(item[0]) == 3:
+                        if int(item[1]) == 1:
+                            d = 2.0
+                        elif int(item[1]) == 2:
+                            d = 2.0
+                        elif int(item[1]) == 3:
+                            d = 2.4
+                        elif int(item[1]) == 4:
+                            d = 2.4
+                        elif int(item[1]) == 5:
+                            d = 2.6
+
+                    elif int(item[0]) == 4 or int(item[0]) == 5:
+                        if int(item[1]) == 1:
+                            d = 3.5
+                        elif int(item[1]) == 2:
+                            d = 3.5
+                        elif int(item[1]) == 3:
+                            d = 3.7
+                        elif int(item[1]) == 4:
+                            d = 3.8
+                        elif int(item[1]) == 5:
+                            d = 4.0
+
+                    score = d *int(item[2])
+                    score = int(score)
+                    
+                    score = '{:,}'.format(score)
+
+                    scorelist.append (str(score))
+
+                    amount += int(d*int(item[2]))
+
+                    
+
+                if len(scorelist) == 1:
+                    scores = scorelist[0]
+                    await message.channel.send(scores)
+                else:
+                    scores= '\n'.join(scorelist)
+                    
+                    await message.channel.send(scores + "\n\n" + "{:,}".format(amount))
+
+                looptime = 0
+                for item in scoremathuser:
+                    if str(item) == str(message.author.id):
+                        del scoremathuser[looptime]
+                        break
+
+                    looptime += 1
+                    
     except:
         pass
 
